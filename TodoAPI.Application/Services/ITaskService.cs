ea5f91e97849295e477d76f7ce5bfc32a2895c0f
vnrabot <vince@megacatstudios.com>
2026-05-02 14:11:02 +0800

    Updated comments on ITaskService

--- TodoAPI.Application/Services/ITaskService.cs
+++ TodoAPI.Application/Services/ITaskService.cs
@@ -6,13 +6,13 @@ namespace TodoAPI.Application.Services
 {
     public interface ITaskService
     {
-        // Pagination paramters to satisfy "1 million tasks"
+        //parameters to satisfy "1 million tasks"
         Task<List<TodoTask>> GetTaskAsync(int page, int limit);
         Task<TodoTask> AddTaskAsync(string title, string details);
         Task<TodoTask?> UpdateTaskAsync(int id, string title, string details);
         Task<bool> DeleteTaskAsync(int id);
 
-        // Drag and drop reordernig
+        //Reordering Tasks
         Task<bool> ReorderTaskAsync(int id, string previousRank, string nextRank);
     }
 }
